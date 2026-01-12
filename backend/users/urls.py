4ed5139b1b1e29b60c116f519272d811bf5b12df
Yuvrajsinh Jadeja <yuvijadeja19@gmail.com>
2026-01-12 21:42:43 +0530

    Remove post method in UserViewSet

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from .views import *
 
-users = UserViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('send-otp/', SendVerificationOTP.as_view()),
@@ -13,6 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('password-reset-otp/', PasswordResetOTP.as_view()),
     path('reset-password/', ResetPassword.as_view()),
-    path('', users, name='users'),
+    path('', UserViewSet.as_view({'get': 'list'}), name='users'),
     path('<int:pk>/', UserViewSet.as_view({'patch': 'partial_update'}), name='update-user')
 ]
